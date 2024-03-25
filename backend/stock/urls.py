da57e85d4c605e7d30dd5bc216bfb9ebc5bbce3c
God'swill Olebu <godswillolebu1234@gmail.com>
2024-03-25 01:12:52 -0700

    Add class 'PaintModify' to update Inventory

--- backend/stock/urls.py
+++ backend/stock/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import PaintList
+from .views import PaintList, PaintModify
 
 
 urlpatterns = [
-    path('', PaintList.as_view(), name='list-create'),
+    path('', PaintList.as_view(), name='paint-list-create'),
+    path('<int:pk>/', PaintModify.as_view(), name = 'paint-patch')
 ]
--- backend/stock/views.py
+++ backend/stock/views.py
@@ -4,7 +4,19 @@ from .models import Paint
 from core.serializers import PaintSerializer
 
 
+
 class PaintList(generics.ListCreateAPIView):
+    """
+    Implements listing and creating of the Paint List in the form of
+    color | availability | inventory
+    """
     queryset = Paint.objects.all()
-    serializer = PaintSerializer
-    pass
+    serializer_class = PaintSerializer
+
+
+class PaintModify(generics.RetrieveUpdateAPIView):
+    """
+    Updates the Inventory
+    """
+    queryset = Paint.objects.all()
+    serializer_class = PaintSerializer
