694df56b60762bb053304129a1452a997ffcd370
God'swill Olebu <godswillolebu1234@gmail.com>
2024-03-25 00:40:38 -0700

    Add Paint model

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Paint(models.Model):
+    PAINT_CHOICES = (
+        ('red', 'Red'),
+        ('grey', 'Grey'),
+        ('black', 'Black'),
+        ('white', 'White'),
+        ('purple', 'Purple'),
+    )
+
+    AVAILABILITY_CHOICES = (
+        ('available', 'Available'),
+        ('out of stock', 'Out of Stock'),
+        ('running low', 'Running Low'),
+    )
+
+    color = models.CharField(max_length = 100, choices = PAINT_CHOICES, default = 'red')
+    availability = models.CharField(max_length = 100, choices = AVAILABILITY_CHOICES, default = 'available')
+    inventory = models.IntegerField()
+
+
+    def __str__(self):
+        return self.color
